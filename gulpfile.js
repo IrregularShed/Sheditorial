a0a320c26e1287059752369bb418ea956fd952bc
Steve Anderson <steve@twindx.com>
2020-02-10 12:01:32 +0000

    De-psycho-ify
    * Spaces, not tabs
    * 2 spaces, not 4

--- gulpfile.js
+++ gulpfile.js
@@ -8,51 +8,51 @@ var zip = require('gulp-zip');
 var beeper = require('beeper');
 
 function serve(done) {
-    livereload.listen();
-    done();
+  livereload.listen();
+  done();
 }
 
 const handleError = (done) => {
-    return function (err) {
-        if (err) {
-            beeper();
-        }
-        return done(err);
-    };
+  return function (err) {
+    if (err) {
+      beeper();
+    }
+    return done(err);
+  };
 };
 
 sass.compiler = require('node-sass');
 
 function hbs(done) {
-    pump([
-        src(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs']),
-        livereload()
-    ], handleError(done));
+  pump([
+    src(['*.hbs', 'partials/**/*.hbs', '!node_modules/**/*.hbs']),
+    livereload()
+  ], handleError(done));
 }
 
 function css(done) {
-    pump([
-        src('./assets/main/sass/*.scss', {sourcemaps: true}),
-        sass({outputStyle: 'compressed'}).on('error', sass.logError),
-        dest('assets/main/css', {sourcemaps: './'}),
-        livereload()
-    ], handleError(done));
+  pump([
+    src('./assets/main/sass/*.scss', {sourcemaps: true}),
+    sass({outputStyle: 'compressed'}).on('error', sass.logError),
+    dest('assets/main/css', {sourcemaps: './'}),
+    livereload()
+  ], handleError(done));
 }
 
 function zipper(done) {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
+  var targetDir = 'dist/';
+  var themeName = require('./package.json').name;
+  var filename = themeName + '.zip';
 
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**'
-        ]),
-        zip(filename),
-        dest(targetDir)
-    ], handleError(done));
+  pump([
+    src([
+      '**',
+      '!node_modules', '!node_modules/**',
+      '!dist', '!dist/**'
+    ]),
+    zip(filename),
+    dest(targetDir)
+  ], handleError(done));
 }
 
 const cssWatcher = () => watch('./assets/main/sass/**/**', css);
